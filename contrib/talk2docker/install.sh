e5933c0eab859c85574f76c132dd85e8441d952b
A.I <ailis@paw.zone>
2015-04-11 21:49:37 -0700

    Revise talk2docker installer with wget

--- contrib/talk2docker/install.sh
+++ contrib/talk2docker/install.sh
@@ -2,17 +2,9 @@
 
 VERSION=1.4.0
 
-pushd `dirname $0` > /dev/null
-HERE=`pwd`
-popd > /dev/null
-
-cd "${HERE}"
-
-docker build -t curl ../curl
-
 cd /tmp
 
-docker run --rm -v $(pwd):/host -w /host curl -OL https://github.com/ailispaw/talk2docker/releases/download/v${VERSION}/talk2docker_${VERSION}_linux_amd64.tar.gz
+sudo wget --no-check-certificate https://github.com/ailispaw/talk2docker/releases/download/v${VERSION}/talk2docker_${VERSION}_linux_amd64.tar.gz
 
 sudo gzip -d talk2docker_${VERSION}_linux_amd64.tar.gz
 sudo tar xf talk2docker_${VERSION}_linux_amd64.tar
