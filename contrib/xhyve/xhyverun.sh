1029151de7012a77116478a6dc98b46c9369c9b1
A.I <ailis@paw.zone>
2015-06-29 19:30:55 -0700

    Remove unnecessary ISO from xhyverun.sh

--- contrib/xhyve/xhyverun.sh
+++ contrib/xhyve/xhyverun.sh
@@ -8,7 +8,7 @@ CMDLINE="earlyprintk=serial console=ttyS0 acpi=off console=tty0 quiet elevator=n
 MEM="-m 1G"
 #SMP="-c 2"
 NET="-s 2:0,virtio-net"
-IMG_CD="-s 3,ahci-cd,rancheros-lite.iso"
+#IMG_CD="-s 3,ahci-cd,rancheros-lite.iso"
 IMG_HDD="-s 4,virtio-blk,rancheros-lite-packer-disk1.raw"
 PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
 LPC_DEV="-l com1,stdio"
