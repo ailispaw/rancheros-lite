472325309bf2d87879ba0c3dea38806909ddf844
A.I <ailis@paw.zone>
2015-07-28 12:41:52 -0700

    Revise talk2docker/install.sh

--- contrib/talk2docker/install.sh
+++ contrib/talk2docker/install.sh
@@ -4,12 +4,11 @@ VERSION=1.5.0
 
 cd /tmp
 
-sudo wget https://github.com/ailispaw/talk2docker/releases/download/v${VERSION}/talk2docker_${VERSION}_linux_amd64.tar.gz
+wget https://github.com/ailispaw/talk2docker/releases/download/v${VERSION}/talk2docker_${VERSION}_linux_amd64.tar.gz
 
-sudo gzip -d talk2docker_${VERSION}_linux_amd64.tar.gz
-sudo tar xf talk2docker_${VERSION}_linux_amd64.tar
+tar zxf talk2docker_${VERSION}_linux_amd64.tar.gz
 
 sudo mkdir -p /opt/bin
 sudo cp talk2docker_${VERSION}_linux_amd64/talk2docker /opt/bin
 
-sudo rm -rf talk2docker_${VERSION}_linux_amd64*
+rm -rf talk2docker_${VERSION}_linux_amd64*
